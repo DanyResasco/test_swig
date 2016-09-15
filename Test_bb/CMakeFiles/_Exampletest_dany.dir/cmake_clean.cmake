44837b198d482424ade5542eba1f32a7711b15a6
DanyResasco <daniela.resasco@gmail.com>
2016-09-15 13:53:26 +0200

    error swig_import_helper()

---
+++ Test_bb/CMakeFiles/_Exampletest_dany.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "Test_interface_danyPYTHON_wrap.cxx"
+  "Test_interface_dany.py"
+  "Test_interface_danyPYTHON_wrap.cxx"
+  "Test_interface_dany.py"
+  "CMakeFiles/_Exampletest_dany.dir/Test_interface_danyPYTHON_wrap.cxx.o"
+  "lib/python/_Exampletest_dany.pdb"
+  "lib/python/_Exampletest_dany.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/_Exampletest_dany.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ Test_bb/main.py
@@ -0,0 +1,23 @@
+import Exampletest_dany_bb	#importo funzione delle bb
+# from klampt import vis 
+# from klampt.vis.glrobotprogram import *	#Per il simulatore
+# from openmesh import *
+
+
+
+box = Exampletest_dany_bb.Exampletest_dany_bb()
+# mesh = TriMesh()
+# read_mesh(mesh, "poisson_mesh.stl")
+# print("mesh n point"), mesh.Point
+
+
+
+ # for ( unsigned int i = 0; i < num_points; ++i )
+ #    {
+
+ #        std::cin >> x;
+ #        std::cin >> y;
+ #        std::cin >> z;
+
+ #        ObjectOriginal.SetPoint ( i, x, y, z );
+ #    }
